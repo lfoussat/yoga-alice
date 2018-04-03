42b872e5be641b025dfd20ac255da7ecb7f61231
Louise Foussat <louisefoussat@gmail.com>
2018-04-03 11:45:44 +0200

    switch to express

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
-const http = require ('http')
+const express = require('express')
 
-const server = http.createServer((req, res) => {
-  res.end(`OK`)
+// pour créer le serveur, on appelle express, juste.
+// Ca nous retourne un server, tout simplement,
+// mais en langage express on appelle ca l'app
+// Mais l'application c'est notre serveur
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('OK')
+
 })
 
-server.listen(5000, () => console.log('J\'écoute sur le port 5000 '))
+const server = app.listen(5000, () => console.log(`j'écoute sur le port 5000`))
+
+// const http = require ('http')
+//
+// const server = http.createServer((req, res) => {
+//   res.end(`OK`)
+// })
+//
+// server.listen(5000, () => console.log('J\'écoute sur le port 5000 '))
