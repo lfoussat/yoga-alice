4e13143ac0489a1f7bd656ef8f445b0126f7606b
Louise Foussat <louisefoussat@gmail.com>
2018-04-03 18:05:05 +0200

    fix response (headers)

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const inspirations = [ inspi1, inspi2, inspi3 ]
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('OK')
 })
