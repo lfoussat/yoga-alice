9a8ba587ba2f66cb5afe7005e7a494d90233b671
Louise Foussat <louisefoussat@gmail.com>
2018-04-03 18:56:30 +0200

    inject inspirations to innerHTML

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,8 @@
 fetch('http://localhost:5300/inspirations-yoga')
   .then(res => res.json())
   .then(inspirations => {
+    const inspirationsElement = document.getElementById('inspirations')
+    inspirationsElement.innerHTML = JSON.stringify(inspirations)
 
     console.log(inspirations)
   })
