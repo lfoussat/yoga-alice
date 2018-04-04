652c9d29d53e55a173bac7bf3ee0e6049782a5c2
Louise Foussat <louisefoussat@gmail.com>
2018-04-05 00:41:40 +0200

    injection of the inspirations in the HTML (part 2) - inspiration file component missing from the precedent commit

---
+++ client/js/components/inspiration.js
@@ -0,0 +1,7 @@
+export const showInspiration = inspiration => `
+  <div class="inspiration">
+    <h2>${inspiration.title}</h2>
+    <img class="thumbnailPic" src="${inspiration.picture}"/>
+    <p>${inspiration.description}</p>
+  </div>
+`
