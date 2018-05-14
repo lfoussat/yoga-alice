755ad8b73791e0d44773203dad99d03837e27bba
Louise Foussat <louisefoussat@gmail.com>
2018-05-14 13:12:10 +0200

    MAJ header

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -3,16 +3,16 @@ const createHeader = () => `
 <div id="yogaAliceNavbar">
   <div id="yogaAliceLogo">
     <img src="../img/logo-alice-test1.png">
-    <a href="index.html">Yoga Alice</a>
+    <a href="/home.html">Yoga Alice</a>
   </div>
   <div id="mainNav">
-    <a href="index.html">ACCUEIL</a>
+    <a href="/home.html">ACCUEIL</a>
     <a href="">YOGA ALICE</a>
-    <a href="">COURS</a>
+    <a href="/cours.html">COURS</a>
     <a href="">STAGES</a>
     <a href="">SHIATSU</a>
-    <a href="">INSPIRATIONS</a>
-    <a href="contact.html">CONTACT</a>
+    <a href="/">INSPIRATIONS</a>
+    <a href="/contact.html">CONTACT</a>
   </div>
   <div id="burgerBtn">
     <a href="#" title="Burger button" id="openNavBtn">&#9776;</span>
@@ -20,13 +20,13 @@ const createHeader = () => `
   <div id="burgerNav" class="overlay">
     <a href="javascript:void(0)" id="closeNavBtn">&times;</a>
     <div class="overlay-content">
-      <a href="index.html">ACCUEIL</a>
+      <a href="/home.html">ACCUEIL</a>
       <a href="">YOGA ALICE</a>
-      <a href="">COURS</a>
+      <a href="/cours.html">COURS</a>
       <a href="">STAGES</a>
       <a href="">SHIATSU</a>
-      <a href="">INSPIRATIONS</a>
-      <a href="contact.html">CONTACT</a>
+      <a href="/">INSPIRATIONS</a>
+      <a href="/contact.html">CONTACT</a>
     </div>
   </div>
 </div>`
