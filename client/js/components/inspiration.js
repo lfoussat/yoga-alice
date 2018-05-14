31ad135bba31939885ce8990e3327352d6937e74
Louise Foussat <louisefoussat@gmail.com>
2018-05-14 13:14:18 +0200

    MAJ inspiration insert

--- client/js/components/inspiration.js
+++ client/js/components/inspiration.js
@@ -1,7 +1,8 @@
 export const createInspirationInsert = inspiration => `
-  <div class="inspiration">
+  <div class="col-xs-12 col-sm-6 col-md-4 inspiration" style="background-color:${inspiration.color}">
     <h2>${inspiration.title}</h2>
-    <img class="thumbnailPic" src="${inspiration.picture}"/>
-    <p>${inspiration.description}</p>
+    <!--<img class="thumbnailPic" src="http://localhost:5300/images/${inspiration.picture}"/>-->
+    <p>${inspiration.smalldescription}</p>
+    <a href='inspiration.html?id=${inspiration.id}'><i class="fas fa-arrow-right"></i></a>
   </div>
 `
